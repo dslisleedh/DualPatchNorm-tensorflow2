d82bb01f0033129cb6e189a8b308777883e509e9
dslisleedh <dslisleedh@gmail.com>
2023-02-17 17:23:15 +0900

    Add models

---
+++ src/layers.py
@@ -0,0 +1,149 @@
+import tensorflow as tf
+import tensorflow_addons as tfa
+
+import einops
+from einops.layers.keras import Rearrange
+import numpy as np
+
+from typing import Sequence, Optional
+
+
+class PatchEmbedding(tf.keras.layers.Layer):
+    def __init__(
+            self, patch_size: Sequence[int], n_filters: int, norm: Optional[str] = None,
+            **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.patch_size = patch_size
+        self.n_filters = n_filters
+        assert norm in [None, 'pre', 'post', 'dual']
+        self.norm = norm
+
+        self.forward = tf.keras.Sequential()
+        self.forward.add(
+            Rearrange('b (h p1) (w p2) c -> b (h w) (p1 p2 c)', p1=patch_size[0], p2=patch_size[1])
+        )
+        if norm in ['pre', 'dual']:
+            self.forward.add(tf.keras.layers.LayerNormalization())
+        self.forward.add(tf.keras.layers.Dense(n_filters))
+        if norm in ['post', 'dual']:
+            self.forward.add(tf.keras.layers.LayerNormalization())
+
+    def call(self, inputs: tf.Tensor, **kwargs) -> tf.Tensor:
+        return self.forward(inputs)
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            "patch_size": self.patch_size,
+            "in_channels": self.in_channels,
+            "out_channels": self.out_channels,
+            "norm": self.norm,
+        })
+        return config
+
+
+class PositionalEncoding(tf.keras.layers.Layer):
+    def build(self, input_shape):
+        pe = np.array(
+            [[(pos / 10000 ** (2 * (i // 2) / input_shape[-1])) for i in range(input_shape[-1])]
+             for pos in range(input_shape[-2])],
+            dtype=np.float32
+        )
+        pe[:, 0::2] = np.sin(pe[:, 0::2])
+        pe[:, 1::2] = np.cos(pe[:, 1::2])
+        self.pe = tf.Variable(pe[np.newaxis, :], trainable=False, dtype=tf.float32, name="positional_encoding")
+
+    def call(self, inputs: tf.Tensor, **kwargs) -> tf.Tensor:
+        return inputs + tf.broadcast_to(self.pe, (tf.shape(inputs)[0], *self.pe.shape[1:]))
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            "pe": self.pe,
+        })
+        return config
+
+
+class MHSA(tf.keras.layers.Layer):
+    def __init__(self, n_heads: int = 8):
+        super(MHSA, self).__init__()
+        self.n_heads = n_heads
+
+    def build(self, input_shape):
+        self.ln = tf.keras.layers.LayerNormalization()
+        self.to_qkv = tf.keras.layers.Dense(input_shape[-1] * 3, use_bias=False)
+        self.to_out = tf.keras.layers.Dense(input_shape[-1], use_bias=False)
+
+    def call(self, inputs: tf.Tensor, **kwargs) -> tf.Tensor:
+        qkv = self.to_qkv(self.ln(inputs))
+        q, k, v = tf.split(qkv, 3, axis=-1)
+        q = einops.rearrange(q, 'b t (h d) -> b h t d', h=self.n_heads)
+        k = einops.rearrange(k, 'b t (h d) -> b h t d', h=self.n_heads)
+        v = einops.rearrange(v, 'b t (h d) -> b h t d', h=self.n_heads)
+
+        attn = tf.matmul(q, k, transpose_b=True) / tf.math.sqrt(tf.cast(tf.shape(k)[-1], tf.float32))
+        attn = tf.nn.softmax(attn, axis=-1)
+        out = tf.matmul(attn, v)
+
+        out = einops.rearrange(out, 'b h t d -> b t (h d)')
+        out = self.to_out(out)
+        return out
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            "n_heads": self.n_heads,
+        })
+        return config
+
+
+class MLP(tf.keras.layers.Layer):
+    def __init__(self, expansion_rate: int = 4):
+        super(MLP, self).__init__()
+        self.expansion_rate = expansion_rate
+
+    def build(self, input_shape):
+        self.ln = tf.keras.layers.LayerNormalization()
+        self.mlp1 = tf.keras.layers.Dense(input_shape[-1] * self.expansion_rate, activation='gelu')
+        self.mlp2 = tf.keras.layers.Dense(input_shape[-1])
+
+    def call(self, inputs: tf.Tensor, **kwargs) -> tf.Tensor:
+        x = self.ln(inputs)
+        x = self.mlp1(x)
+        x = self.mlp2(x)
+        return x
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            "expansion_rate": self.expansion_rate,
+        })
+        return config
+
+
+class ViTBlock(tf.keras.layers.Layer):
+    def __init__(self, n_heads: int, expansion_rate: int, drop_rate: Sequence[float]):
+        super(ViTBlock, self).__init__()
+        self.n_heads = n_heads
+        self.expansion_rate = expansion_rate
+        self.drop_rate = drop_rate
+
+        self.spatial_drop = tfa.layers.StochasticDepth(float(1. - drop_rate[0]))
+        self.mhsa = MHSA(n_heads)
+        self.channel_drop = tfa.layers.StochasticDepth(float(1. - drop_rate[1]))
+        self.mlp = MLP(expansion_rate)
+
+    def call(self, inputs, **kwargs) -> tf.Tensor:
+        x = self.spatial_drop([inputs, self.mhsa(inputs)])
+        x = self.channel_drop([x, self.mlp(x)])
+        return x
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            "n_heads": self.n_heads,
+            "expansion_rate": self.expansion_rate,
+            "drop_rate": self.drop_rate,
+        })
+        return config
---
+++ src/model.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+import tensorflow_addons as tfa
+
+import einops
+
+from src.layers import *
+
+
+class ViT(tf.keras.models.Model):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        self.patch_embedding = PatchEmbedding(
+                config.patch_size, config.n_filters, config.norm
+        )
+        self.cls_token = tf.Variable(
+            initial_value=tf.random.normal((1, 1, config.n_filters)),
+            trainable=True, name='cls_token', dtype=tf.float32
+        )
+        self.positional_encoding = PositionalEncoding()
+        drop_rates = tf.reshape(
+            tf.linspace(0., config.drop_rate, config.n_blocks * 2), (config.n_blocks, 2)
+        )
+        self.feature_extractor = tf.keras.Sequential([
+            ViTBlock(config.n_heads, config.expansion_rate, drop_rates[i])
+            for i in range(config.n_blocks)
+        ] + [
+            tf.keras.layers.GlobalAvgPool1D(),
+            tf.keras.layers.LayerNormalization()
+        ])
+        self.classifier = tf.keras.layers.Dense(
+            config.n_labels, kernel_initializer=tf.keras.initializers.zeros
+        )
+
+    def call(self, inputs: tf.Tensor, **kwargs) -> tf.Tensor:
+        patch = self.patch_embedding(inputs)
+        feats = self.positional_encoding(
+            tf.concat(
+                [tf.broadcast_to(self.cls_token, (tf.shape(inputs)[0],) + (1,) + (self.config.n_filters,)), patch],
+                axis=1
+            )
+        )
+        feats = self.feature_extractor(feats)
+        y_hat = self.classifier(feats)
+        return y_hat
+    
